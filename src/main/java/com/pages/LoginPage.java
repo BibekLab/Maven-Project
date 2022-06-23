d884e6518059ad66e8594ee3beb245f378372f18
ViP3RPRO <chingapinga2@gmail.com>
2022-06-24 02:10:58 +0530

    Cucumber BDD with Maven 50min

--- src/main/java/com/pages/LoginPage.java
+++ src/main/java/com/pages/LoginPage.java
@@ -1,4 +1,29 @@
 package com.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+
+    private WebDriver driver;
+
+    //1. By Locators:
+    private By emailId= By.id("email");
+    private By password= By.id("passwd");
+    private By signInButton= By.id("SubmitLogin");
+
+    //2. Constructor of the page class:
+    public LoginPage(WebDriver driver){
+        this.driver=driver;
+
+    }
+
+    //3. page actions: features of the page, in the form of methods:
+
+    public String getLoginPageTitle(){
+        driver.getTitle();
+    }
+
+    public void
+
 }
